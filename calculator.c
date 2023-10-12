597d2c1b913cce428a23ef7b8a835ece874c1bd5
aaditya63official <147714458+aaditya63official@users.noreply.github.com>
2023-10-12 12:04:10 +0000

    fixed the issue

--- calculator.c
+++ calculator.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void printBinary(int num) {
+    int bits[32];
+    int i = 0;
+    while (num > 0) {
+        bits[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    printf("Binary: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", bits[j]);
+    }
+    printf("\n");
+}
+
 int main() {
     int x, y;
     printf("Enter 1st number: ");
@@ -11,33 +27,37 @@ int main() {
 
     printf("1. +\n2. -\n3. *\n4. /\n");
     printf("Enter operation from the following: ");
-    scanf(" %c", &operation);  
+    scanf(" %c", &operation);
 
-    int c;  
+    int c;
 
     switch (operation) {
         case '+':
             c = x + y;
-            printf("Sum is %d", c);
+            printf("Sum is %d\n", c);
+            printBinary(c);
             break;
         case '-':
             c = x - y;
-            printf("Subtraction is %d", c);
+            printf("Subtraction is %d\n", c);
+            printBinary(c);
             break;
         case '*':
             c = x * y;
-            printf("Multiplication is %d", c);
+            printf("Multiplication is %d\n", c);
+            printBinary(c);
             break;
         case '/':
             if (y != 0) {
                 c = x / y;
-                printf("Division is %d", c);
+                printf("Division is %d\n", c);
+                printBinary(c);
             } else {
-                printf("Cannot divide by zero");
+                printf("Cannot divide by zero\n");
             }
             break;
         default:
-            printf("Invalid input");
+            printf("Invalid input\n");
             break;
     }
 
